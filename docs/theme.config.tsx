a8a57ecc542f755a2bf9e2b8b43a1cdd5615d639
NM <126906698+Kamikaza731@users.noreply.github.com>
2024-01-10 22:10:07 +0100

    Update v1.04

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -25,6 +25,17 @@ const config: DocsThemeConfig = {
     }
   },
   logo,
+  chat: {
+    link: 'https://twitter.com/cogwheel_val',
+    icon: (
+      <svg width="24" height="24" viewBox="0 0 248 204">
+        <path
+          fill="currentColor"
+          d="M221.95 51.29c.15 2.17.15 4.34.15 6.53 0 66.73-50.8 143.69-143.69 143.69v-.04c-27.44.04-54.31-7.82-77.41-22.64 3.99.48 8 .72 12.02.73 22.74.02 44.83-7.61 62.72-21.66-21.61-.41-40.56-14.5-47.18-35.07a50.338 50.338 0 0 0 22.8-.87C27.8 117.2 10.85 96.5 10.85 72.46v-.64a50.18 50.18 0 0 0 22.92 6.32C11.58 63.31 4.74 33.79 18.14 10.71a143.333 143.333 0 0 0 104.08 52.76 50.532 50.532 0 0 1 14.61-48.25c20.34-19.12 52.33-18.14 71.45 2.19 11.31-2.23 22.15-6.38 32.07-12.26a50.69 50.69 0 0 1-22.2 27.93c10.01-1.18 19.79-3.86 29-7.95a102.594 102.594 0 0 1-25.2 26.16z"
+        />
+      </svg>
+    )
+  },
   head: () => {
     const { asPath, defaultLocale, locale } = useRouter()
     const { frontMatter } = useConfig()
@@ -40,6 +51,7 @@ const config: DocsThemeConfig = {
           property="og:description"
           content={frontMatter.description || 'Docs page from Cogwheel Validator!'}
         />
+        <meta name="og:image" content="/public/og_twitter.jpg"/>      
         <meta name="msapplication-TileColor" content="#fff" />
         <meta name="theme-color" content="#fff" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -49,10 +61,9 @@ const config: DocsThemeConfig = {
           content={frontMatter.description || 'Docs page from Cogwheel Validator!'}
         />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="/public/OG Twitter.jpg" />
-        <meta name="twitter:site:domain" content="Docs Cogwheel" />
-        <meta name="twitter:url" content="https://docs.cogwheel.zone" />
-        <meta name="og:image" content="/public/OG Twitter.jpg"/>
+        <meta name="twitter:image" content="/public/og_twitter.jpg" />
+        <meta name="twitter:site:domain" content="docs.cogwheel.zone" />
+        <meta name="twitter:url" content={url} />
         <meta name="apple-mobile-web-app-title" content="Docs Cogwheel" />
         <link rel="icon" href="public/favicon.ico" type="image/ico" />
       </>
@@ -96,7 +107,7 @@ const config: DocsThemeConfig = {
           </a>
         </div>
         <p className="mt-6 text-xs">
-          © {new Date().getFullYear()} Cogwheel Validator.
+          © {new Date().getFullYear()} Cogwheel Validator
         </p>
       </div>
     )
